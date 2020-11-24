8324ee1d6e4cf9642693b5c21af367ab9bfe6f8c
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-11-24 11:04:28 +0200

    DFB

---
+++ algo/BFS.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+
+def findSortestPath(name):
+    search_queue = deque()
+    search_queue += graph['you']
+    visited = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in visited:
+            if(person == name):
+                print visited
+                return True
+            else:
+                search_queue += graph[person]
+                visited.append(person)
+    return False
+
+
+print findSortestPath('jonny')
